a623661d2296274d179a8ba7bcd43637a6e4fd8c
mishingo <mr@mishingo.com>
2014-10-14 16:25:00 -0400

    changed pw in config

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php 
 global $db_user, $db_pass, $db,$db_server, $db_connection;
 $db_user = "mi249120";
-$db_pass = "bazooka1";
+$db_pass = "parvenu123";
 $db = "mi249120";
-$db_server = "localhost";
+$db_server = "sulley.cah.ucf.edu";
 
 
 $db_connection = false;
