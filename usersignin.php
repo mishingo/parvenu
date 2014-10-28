b5226a6539344b2ec01ee19ebde4c9e0e1be70d5
chris-judge <Chris_Judge941@yahoo.com>
2014-10-28 16:21:50 -0400

    Commented code.

--- usersignin.php
+++ usersignin.php
@@ -4,35 +4,44 @@
 
 			<?php 
 
-				get_dbconnection();
-
+				//Grab Forum Data
 				$useremail=$_POST['siemail']; 
 				$userpassword=$_POST['sipassword']; 
 
+				//Open database connection
+				get_dbconnection();
+
+				//Strip Slashes
 				$useremail = stripslashes($useremail);
 				$userpassword = stripslashes($userpassword);
 
+				//Real Escape String
 				$useremail = mysql_real_escape_string($useremail);
 				$userpassword = mysql_real_escape_string($userpassword);
 
+				//Create and run SQL query
 				$sql="SELECT password FROM Users WHERE email='$useremail'";
 				$result = mysqli_query(get_dbconnection(),$sql);
 
 
-
+				//Check for results, if yes (test password), if no (the account doesn't exist).
 				if(mysqli_num_rows($result)){
-
+					//Get hash from database
 					$row = mysqli_fetch_array($result);
 					$hash = $row[0];
 
+					//Load hashing library
 					require 'PasswordHash.php';
 
+					//Assign hash guidelines
 					$hash_cost_log2 = 8;
 					$hash_portable = FALSE;
 					$hasher = new PasswordHash($hash_cost_log2, $hash_portable);
-					$check = $hasher->CheckPassword($userpassword, $hash);
-					if ($check) $ok++;
 
+					//Check hashed password from database against user input
+					$check = $hasher->CheckPassword($userpassword, $hash);
+
+					// If they are correct.
 					if ($check == 1) {
 						session_start();
 						session_unset(); 
--- usersignup.php
+++ usersignup.php
@@ -4,38 +4,47 @@
 
 			<?php 
 
-				get_dbconnection();
-
+				//Grab Forum Data
 				$useremail=$_POST['email']; 
 				$userpassword=$_POST['password']; 
 				$userfname=$_POST['firstname']; 
 				$userlname=$_POST['lastname']; 
 
+				// Open database connection
+				get_dbconnection();
+
+				//Strip Slashes
 				$useremail = stripslashes($useremail);
 				$userpassword = stripslashes($userpassword);
 				$userfname = stripslashes($userfname);
 				$userlname = stripslashes($userlname);
 
+				//Real Escape String
 				$useremail = mysql_real_escape_string($useremail);
 				$userpassword = mysql_real_escape_string($userpassword);
 				$userfname = mysql_real_escape_string($userfname);
 				$userlname = mysql_real_escape_string($userlname);
 
+				//Load Password hashing.
 				require 'PasswordHash.php';
+
+				//Assign hash guidelines
 				$hash_cost_log2 = 8;
 				$hash_portable = FALSE;
 
+				//Create hash
 				$hasher = new PasswordHash($hash_cost_log2, $hash_portable);
 				$hash = $hasher->HashPassword($userpassword);
 				if (strlen($hash) < 20)
 					echo 'Failed to hash new password.';
 				unset($hasher);
 
+				//Create and run SQL query
 				$sql="SELECT * FROM Users WHERE Email='$useremail'";
 				$result = mysqli_query(get_dbconnection(),$sql);
 
+				//Check if for results, if they exist (That account names is used already), if not (Insert information into table).
 				if(mysqli_num_rows($result)){
-
 					echo "You already have an account!";
 
 				}else{
